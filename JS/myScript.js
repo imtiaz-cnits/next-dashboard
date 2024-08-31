7b38619d9d3bec918064306922790ba806ac1ccd
shahreeyarrahman <shahreeyar.cnits@gmail.com>
2024-08-31 18:08:45 +0600

    sidebar without responsive

--- JS/myScript.js
+++ JS/myScript.js
@@ -15,4 +15,30 @@ $(".menu-btn").click(function () {
   // Toggle the 'active' class on the sidebar
   $(".sidebar").toggleClass("active");
   // $(".sidebar .card").css("display", "none")
+  $(".content").toggle("fast");
+  // $(".content").toggle("fast"); MY JS--
 });
+
+
+
+// $(".menu > ul > li").click(function (e) {
+//   // Remove the 'active' class from other menu items
+//   $(this).siblings().removeClass("active");
+//   // Toggle the 'active' class on the clicked menu item
+//   $(this).toggleClass("active");
+//   // Toggle the visibility of the submenu
+//   $(this).find("ul").slideToggle();
+//   // Close other submenus if they are open
+//   $(this).siblings().find("ul").slideUp();
+//   // Remove the 'active' class from submenu items
+//   $(this).siblings().find("ul").find("li").removeClass("active");
+// });
+
+// $(".menu-btn").click(function () {
+//   // Toggle the 'active' class on the sidebar
+//   $(".sidebar").toggleClass("active");
+//   // $(".sidebar .card").css("display", "none")
+// });
+
+
+
